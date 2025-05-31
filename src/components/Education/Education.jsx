ee3819f923c68d93d4d7f097e05ec161068addcc
Kumudini Gholap <155129488+kumudini7@users.noreply.github.com>
2025-05-31 12:31:55 +0530

    Add files via upload

---
+++ src/components/Education/Education.jsx
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import classes from './Education.module.css';
+// import ScrollAnimation from 'react-animate-on-scroll';
+import 'animate.css/animate.min.css';
+import { MdSchool } from 'react-icons/md';
+import { MdWork } from 'react-icons/md';
+import { FaSchool } from 'react-icons/fa';
+
+class Education extends Component {
+  render() {
+    return (
+      <div className={classes.box} id='education'>
+        {/* <ScrollAnimation
+          offset={0}
+          animateIn='fadeInLeft'
+          duration={2}
+          animateOnce={true}
+          initiallyVisible={true}
+        > */}
+        <span className={classes.head}>MY JOURNEY</span>
+        <section className={classes.container}>
+          <div>
+            <div className={classes.row}>
+              <div className={classes.row_md_12}>
+                <div className={classes.timeline_centered}>
+                  {/* <ScrollAnimation
+                      offset={0}
+                      animateIn='fadeInLeft'
+                      duration={2.4}
+                      animateOnce={true}
+                      initiallyVisible={true}
+                    > */}
+                 
+                  {/* </ScrollAnimation> */}
+                  {/* <ScrollAnimation
+                      offset={0}
+                      animateIn='fadeInLeft'
+                      duration={2.4}
+                      animateOnce={true}
+                      initiallyVisible={true}
+                    > */}
+                  <article className={classes.timeline_entry}>
+                    <div className={classes.timeline_icon}>
+                      <MdSchool />
+                    </div>
+                    <div className={classes.label}>
+                      <h2>
+                        Undergraduation at NIT SURAT <span>2022-2026</span>
+                      </h2>
+                      <div className={classes.description}>
+                        I pursuing my undergraduation in CSE (Computer Science and Engineering)
+                        from <a href='http://svnit.ac.in/'>SVNIT</a>.
+                      </div >
+                    </div>
+                  </article>
+                  <article className={classes.timeline_entry}>
+                    <div className={classes.timeline_icon}>
+                      <MdSchool />
+                    </div>
+                    <div className={classes.label}>
+                      <h2>
+                        Programming and Data Science <span>2023-2027</span>
+                      </h2>
+                      <div className={classes.description}>
+                        I pursuing my Online BS Degree in  Programming and Data Science 
+                        from <a href='https://study.iitm.ac.in/ds/'>IIT Madras</a>.
+                      </div >
+                    </div>
+                  </article>
+                  {/* </ScrollAnimation> */}
+                  {/* <ScrollAnimation
+                      offset={0}
+                      animateIn='fadeInLeft'
+                      duration={2.4}
+                      animateOnce={true}
+                      initiallyVisible={true}
+                    > */}
+                 
+                  {/* </ScrollAnimation> */}
+
+                  {/* <ScrollAnimation
+                      offset={0}
+                      animateIn='fadeInLeft'
+                      duration={2.4}
+                      animateOnce={true}
+                      initiallyVisible={true}
+                    > */}
+                  
+                  {/* </ScrollAnimation> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* </ScrollAnimation> */}
+      </div>
+    );
+  }
+}
+
+export default Education;
